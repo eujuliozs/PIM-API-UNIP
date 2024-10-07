50aef9ddf728d0739a5b9685d60c5b7da1da6fff
eujuliozs <julios100305@gmail.com>
2024-10-07 12:01:02 -0300

    inicial

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace PIM2024_2SEMESTRE
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
